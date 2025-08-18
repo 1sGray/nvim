2b7d3c355e392e6c333641a320b2d795a2c2b95a
gray <1s_gray@proton.me>
2025-08-18 21:39:19 +0000

    idk

--- init.lua
+++ init.lua
@@ -1,21 +1,31 @@
---require("config.options")
---require("config.keymaps")
+-- require("config.options")
+-- require("config.keymaps")
 -- require("config.autocommands")
 -- require("config.personal-plugins.floaterminal")
 
+-- ### Globals ### --
+-- Set leader globally and locally Prefered:<Space>
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+-- Set Nerdfonts if installed and enabled in term
+vim.g.have_nerd_font = true
+
 -- ### Plugins ### --
 vim.pack.add({
-  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
+  --  { src = ""},
+  --  { src = ""},
+  --  { src = ""},
+  -- LSP Plugins
   { src = "https://github.com/neovim/nvim-lspconfig" },
-  --  { src = ""},
-  --  { src = ""},
-  --  { src = ""},
+  -- Theme Plugins
+  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
+  -- QOL Plugins
 })
 
 -- ### LSPs ### --
+vim.lsp.enable({ "lua_ls", "rust_analyzer", })
 -- lua_ls
 require "lspconfig".lua_ls.setup {}
-
 -- rust-analyzer
 require "lspconfig".rust_analyzer.setup({})
 
@@ -23,11 +33,9 @@ require "lspconfig".rust_analyzer.setup({})
 -- Themes
 vim.cmd("colorscheme gruvbox")
 -- Theme Plugins
-
 -- Searching and Patterns                                                                                                                    vim.opt.incsearch = true -- highlight all matches while incrementally searching
 -- vim.opt.ingnorecase = true -- ignore case in search patterns -- TODO: for some reason error when enabled
-vim.opt.smartcase = true -- override 'ignorecase' when pattern has upper case characters
-
+vim.opt.smartcase = true      -- override 'ignorecase' when pattern has upper case characters
 -- Text Display
 vim.opt.scrolloff = 10        -- minimal number of screen lines to keep above and below the cursor, changes scorll offseet to `10`, which gives padding to view previous and upcoming code lines from the cursor position
 vim.opt.sidescrolloff = 15    -- minimal number of columns to keep left and right of the cursor
@@ -36,42 +44,31 @@ vim.opt.cmdheight = 3         -- number of lines used for the command-line
 -- Line Number
 vim.opt.number = true         -- Enable Line Numbers
 vim.opt.relativenumber = true -- show the relative line number for each line
-
 -- Syntax, Highlighting and Spelling
-vim.opt.hlsearch = true      -- highlight all matches for the last used search pattern
-vim.opt.termguicolors = true -- use GUI colors for the terminal
-
+vim.opt.hlsearch = true       -- highlight all matches for the last used search pattern
+vim.opt.termguicolors = true  -- use GUI colors for the terminal
 -- Windows
-vim.opt.splitbelow = true -- a new window is put below the current one
-vim.opt.splitright = true -- a new window is put right of the current one
-
+vim.opt.splitbelow = true     -- a new window is put below the current one
+vim.opt.splitright = true     -- a new window is put right of the current one
 -- Mouse
-vim.opt.mouse = "a" -- list of flags for using the mouse
-
+vim.opt.mouse = "a"           -- list of flags for using the mouse
 -- Text Editing
-vim.opt.undofile = true -- automatically save and restore undo history
-
+vim.opt.undofile = true       -- automatically save and restore undo history
 -- Tabs and Indenting
-vim.opt.tabstop = 2      -- number of spaces a <Tab> in the text stands for
-vim.opt.shiftwidth = 2   -- number of spaces used for each step of (auto)indent
-vim.opt.expandtab = true -- expand <Tab> to spaces in Insert mode
-
+vim.opt.tabstop = 2           -- number of spaces a <Tab> in the text stands for
+vim.opt.shiftwidth = 2        -- number of spaces used for each step of (auto)indent
+vim.opt.expandtab = true      -- expand <Tab> to spaces in Insert mode
 -- Folding
 vim.opt.foldmethod = "marker"
 
 -- ### Keymaps ### --
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>to", function() vim.opt.scrolloff = 999 - vim.o.scrolloff end,
-  { desc =
-  "A toggle that sets scroll offset super high which keeps your cursor at the middle of the screen and scrolls the content around it" })
+  {
+    desc =
+    "A toggle that sets scroll offset super high which keeps your cursor at the middle of the screen and scrolls the content around it"
+  })
 vim.keymap.set("n", "<leader>cu", ":update<CR> :source<CR>",
   { desc = "Updates current file and sources it, mainly nvim config files." })
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Writes buffer to file." })
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "exits to normal mode in the terminal" })
-
--- Telescope Keymaps                                                                                                                         -- local builtin = require('telescope.builtin')
--- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
